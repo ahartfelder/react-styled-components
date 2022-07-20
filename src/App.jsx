7330c766fcec284a69df62336e2a0e068bb94ecd
ahartfelder <ahartfelder@gmail.com>
2022-07-20 11:00:48 -0300

    global styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,58 @@
 import './App.css';
 import './styles.css'
 import logo from './logo.svg'
-import StyledButton, { AnimatedLogo, FancyButton, SubmitButton } from './components/Button/Button';
+import StyledButton, { AnimatedLogo, DarkButton, FancyButton, SubmitButton } from './components/Button/Button';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+
+const theme = {
+  dark: {
+    primary: '#000',
+    text: '#fff'
+  },
+  light: {
+    primary: '#fff',
+    text: '#000'
+  },
+  fontFamily: 'Segoe UI'
+}
+
+const GlobalStyle = createGlobalStyle`
+  button {
+    /* font-family: 'Roboto'; */
+    font-family: ${props => props.theme.fontFamily};
+  }
+`
 
 function App() {
   return (
-    <div className="App">
-      {/* <img src={logo} className='App-logo' alt="logo" /> */}
-      <AnimatedLogo src={logo} />
-      <div>
-        <br />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <div className="App">
+        {/* <img src={logo} className='App-logo' alt="logo" /> */}
+        <AnimatedLogo src={logo} />
+        <div>
+          <br />
+        </div>
+        <StyledButton type='submit'>Styled Button</StyledButton>
+        <div>
+          <br />
+        </div>
+        <StyledButton variant='outline'>Styled Button</StyledButton>
+        <div>
+          <br />
+        </div>
+        <FancyButton as='a'>Fancy Button</FancyButton>
+        <div>
+          <br />
+        </div>
+        <SubmitButton>Submit</SubmitButton>
+        <div>
+          <br />
+        </div>
+        <DarkButton>Dark Theme</DarkButton>
       </div>
-      <StyledButton type='submit'>Styled Button</StyledButton>
-      <div>
-        <br />
-      </div>
-      <StyledButton variant='outline'>Styled Button</StyledButton>
-      <div>
-        <br />
-      </div>
-      <FancyButton as='a'>Fancy Button</FancyButton>
-      <div>
-        <br />
-      </div>
-      <SubmitButton>Submit</SubmitButton>
-    </div>
+    </ThemeProvider>
+    
   );
 }
 
--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,4 +1,4 @@
-import { FancyButton, StyledButton, SubmitButton, AnimatedLogo } from "./Button.styles"
+import { FancyButton, StyledButton, SubmitButton, AnimatedLogo, DarkButton } from "./Button.styles"
 
 export default StyledButton
-export { FancyButton, SubmitButton, AnimatedLogo }
+export { FancyButton, SubmitButton, AnimatedLogo, DarkButton }
